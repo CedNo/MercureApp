7ce19bb2223d1747c0d0c6a22b99f81ea6ebd77f
Francis <francis.blanchette@live.fr>
2022-03-04 10:59:16 -0500

    Début de la manette

--- app/src/main/java/com/mercure/app/ui/remote/RemoteFragment.java
+++ app/src/main/java/com/mercure/app/ui/remote/RemoteFragment.java
@@ -2,16 +2,21 @@ package com.mercure.app.ui.remote;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mercure.app.R;
 import com.mercure.app.databinding.FragmentRemoteBinding;
 
 public class RemoteFragment extends Fragment
@@ -19,6 +24,27 @@ public class RemoteFragment extends Fragment
 
     private RemoteViewModel remoteViewModel;
     private FragmentRemoteBinding binding;
+
+    Button btStop;
+    ImageButton btAvancer, btReculer, btAvDroit, btAvGauche, btArDroit,btArGauche;
+    Switch startAutoMode, startVideo;
+    VideoView carVideo;
+
+    public RemoteFragment()
+    {
+
+    }
+
+    public static RemoteFragment newInstance()
+    {
+        return new RemoteFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
@@ -30,6 +56,139 @@ public class RemoteFragment extends Fragment
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        btStop        = view.findViewById(R.id.btStop);
+        btAvancer     = view.findViewById(R.id.btAvancer);
+        btAvDroit     = view.findViewById(R.id.btAvDroit);
+        btAvGauche    = view.findViewById(R.id.btAvGauche);
+        btArDroit     = view.findViewById(R.id.btArDroit);
+        btArGauche    = view.findViewById(R.id.btArGauche);
+        btReculer     = view.findViewById(R.id.btReculer);
+        startAutoMode = view.findViewById(R.id.startAutoMode);
+        startVideo    = view.findViewById(R.id.startVideo);
+        carVideo      = view.findViewById(R.id.carVideo);
+
+        btAvancer.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    stop();
+                }
+
+                return false;
+            }
+        });
+        btAvDroit.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    stop();
+                }
+
+                return false;
+            }
+        });
+        btAvGauche.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    stop();
+                }
+
+                return false;
+            }
+        });
+        btArGauche.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    stop();
+                }
+
+                return false;
+            }
+        });
+        btArDroit.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    stop();
+                }
+
+                return false;
+            }
+        });
+        btReculer.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    stop();
+                }
+
+                return false;
+            }
+        });
+        btStop.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                stop();
+            }
+        });
+    }
+
+    private void stop()
+    {
+
     }
 
     @Override

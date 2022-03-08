1b652834ae2401c12731464c5ce7fff13aeba692
Jeremy <jeremylord98@hotmail.com>
2022-03-08 10:33:23 -0500

    push page cnx avec nv bt

--- app/src/main/java/com/mercure/app/Activity_Cnx.java
+++ app/src/main/java/com/mercure/app/Activity_Cnx.java
@@ -3,6 +3,10 @@ package com.mercure.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
 
 public class Activity_Cnx extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class Activity_Cnx extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cnx);
+
+        CircularProgressButton btn = (CircularProgressButton) findViewById(R.id.btn_id);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn.startAnimation();
+            }
+        });
+
+
     }
 }
